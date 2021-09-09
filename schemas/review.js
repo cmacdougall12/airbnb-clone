c57ed4d4f0fcce023f034e06a3c458a6052ac519
cmacdougall12 <cammacdougall12@gmail.com>
2021-09-09 11:56:19 -0600

    fixed bug related to the rating system

--- schemas/review.js
+++ schemas/review.js
@@ -19,11 +19,11 @@ export default {
       type: "string",
       options: {
         list: [
-          { title: "5 Starts", value: "5-starts" },
-          { title: "5 Starts", value: "5-starts" },
-          { title: "5 Starts", value: "5-starts" },
-          { title: "5 Starts", value: "5-starts" },
-          { title: "5 Starts", value: "5-starts" },
+          { title: "5 Stars", value: "5-stars" },
+          { title: "4 Stars", value: "4-stars" },
+          { title: "3 Stars", value: "3-stars" },
+          { title: "2 Stars", value: "2-stars" },
+          { title: "1 Stars", value: "1-stars" },
         ],
         layout: 'radio'
       },
